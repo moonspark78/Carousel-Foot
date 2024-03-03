a94adf6e63102299fe9cb23b4e9d5fbdac7131c7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-03 23:37:35 +0100

    done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import "./style.css"
 
 export const Main = ({data}) => {
+    const [slide, setSlide] = useState(0);
     console.log(data);
+
+    const prevSlide = () => {
+        setSlide(slide === 0 ? data.length -1 : slide -1)
+    };
+
+
+    const nextSlide = () => {
+        setSlide(slide === data.length - 1 ? 0 : slide + 1)
+    };
 
 
 
   return (
 
     <div className='carousel'>
+      <FaArrowLeft className='arrow arrow-left' onClick={prevSlide}/>
         {
             data.map((item,idx) =>{
                 return (
-                    <img src={item.src} alt={item.alt} key={idx}/>
+                    <img
+                            className={slide === idx ? "slide" : "slide slide-hidden"}
+                            src={item.src} 
+                            alt={item.alt} 
+                            key={idx}
+                            
+                    />
                 )
             })
         }
+      <FaArrowRight className='arrow arrow-right' onClick={nextSlide}/>
+      <span className="indicators">
+        {
+            data.map((_,idx) => {
+                return <button key={idx} onClick={() => setSlide(idx)}  className={slide === idx ? "indicator" : "indicator indicator-inactive"}></button>
+            })
+        }
+      </span>
     </div>
   )
 }
