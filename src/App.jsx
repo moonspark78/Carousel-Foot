1dbc5eae6837a226fc2a4e4656a9dc63fb8d8f82
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-03 20:55:09 +0100

    display the images on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import { Main } from "./components/Main";
+import slides from "./data/CarouselData.json"
 
 function App() {
   return (
     <div className="App">
-        <Main/>
+        <Main data={slides.slides}/>
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-export const Main = () => {
+export const Main = ({data}) => {
+    console.log(data);
+
 
 
   return (
-    
-    <div>Main</div>
+
+    <div className='carousel'>
+        {
+            data.map((item,idx) =>{
+                return (
+                    <img src={item.src} alt={item.alt} key={idx}/>
+                )
+            })
+        }
+    </div>
   )
 }
+
